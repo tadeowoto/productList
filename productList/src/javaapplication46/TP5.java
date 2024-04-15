f064e157c8022e8b5d471205cf4f4f984b56ece9
Adan-Coronel <adanjhoel@gmail.com>
2024-04-14 22:25:39 -0300

    Correcciones generales y realizacion del jtable. commit final

---
+++ productList/src/javaapplication46/TP5.java
@@ -0,0 +1,19 @@
+package javaapplication46;
+public class TP5 {
+//Crea una aplicación GUI en Java que gestione una lista de productos. La interfaz debe contener los siguientes elementos:
+//Un ComboBox (JComboBox) que permita al usuario seleccionar una categoría de producto de una lista predefinida (por ejemplo, electrónica, ropa, alimentos, etc.).
+//Un botón "Agregar producto" (JButton) que permita al usuario agregar un nuevo producto a la lista.
+//Una tabla (JTable) que muestre los productos agregados, con columnas para el nombre del producto, la categoría y el precio.
+//Cuando el usuario seleccione una categoría en el ComboBox y haga clic en el botón "Agregar producto", el programa debe permitir al
+//usuario ingresar el nombre y el precio del producto en campos de texto separados. Luego, debe agregar el producto a la tabla con la
+//categoría seleccionada.
+
+//Además, la tabla debe ser actualizada automáticamente cada vez que se agrega un nuevo producto.
+//
+//Subir proyecto a GitHub y enviar link del repositorio.
+
+ 
+    public static void main(String[] args) {
+  
+    }  
+}
